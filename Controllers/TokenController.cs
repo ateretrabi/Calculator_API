bdf4bdbff1b8a1700ae369542d45894279564a0a
Ateret Rabi Davidovich <atrabi@g.jct.ac.il>
2022-01-29 23:03:19 +0200

    Initial Commit

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebCalculator.IO.Models;
+using WebCalculator.IO.Services;
+
+namespace WebCalculator.IO.Controllers
+{
+    [Route("ateretrabi/WebCalculator/1.0.0/[Controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public TokenController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public string GetRandomToken()
+        {
+            var jwt = new JwtServices(_config);
+            var token = jwt.GenerateSecurityToken();
+            return token;
+        }
+    }
+}
---
+++ Models/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCalculator.IO.Models
+{
+    public class Result
+    {
+        public decimal? value { get; set; }
+    }
+}
